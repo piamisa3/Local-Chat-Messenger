6e40138c46e7c4fbad2d2c6ac6eac8f68b69bc31
misakifuruya <m2s4k.msk24@gmail.com>
2025-01-19 15:58:16 +0900

    udp-client.pyの修正: メッセージの入力方法を改善し、ソケットファイルのパスを修正

--- udp-client.py
+++ udp-client.py
@@ -1,25 +1,35 @@
 import socket
+import sys
+import os
+
+if len(sys.argv) > 1:
+    message = sys.argv[2]
+else:
+    message = input('pleas imput message!')
 
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 
-server_address = '/udp_socket_file'
+server_address = '/tmp/udp_socket_file'
 
-address = '/udp_client_socket_file'
+address = '/tmp/udp_client_socket_file'
 
-message = b'Message to send t the client.'
+try:
+    os.unlink(address)
+except FileNotFoundError:
+    pass
 
 sock.bind(address)
 
 try:
     print('sending {!r}'.format(message))
-    sent = sock.sendto(message, server_address)
+    sent = sock.sendto(message.encode(), server_address)
 
-    print('waiting to receive')
+    print('waiting to receive..................')
 
     data, server = sock.recvfrom(4096)
 
-    print('received {!r}'.format(data))
+    print('received {!r}'.format(data.decode('utf-8')))
 
 finally:
     print('closing socket')
-    sock.close() 
+    sock.close()
